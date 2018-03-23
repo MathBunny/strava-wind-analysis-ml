9e6643fc277fc3f476ce44d9e6b31d476ad1e37b
MathBunny <horatiulazu@gmail.com>
2018-03-22 22:51:30 -0400

    Update script to fetch segments

--- util/fetch_segments.py
+++ util/fetch_segments.py
@@ -1,33 +1,42 @@
 from config import *
-import json, ast
-import urllib2
+import json, ast, urllib2
 
+
+activities = {}
 segments = []
 
-contents = urllib2.urlopen("https://www.strava.com/api/v3/athlete/activities?include_all_efforts=true&per_page=200&access_token=" + ACCESS_TOKEN).read()
-data = json.loads(contents)
+def get_activities(per_page):
+    if per_page == None:
+        per_page = 10
+    per_page = str(per_page)
+    contents = urllib2.urlopen("https://www.strava.com/api/v3/athlete/activities?include_all_efforts=true&per_page=" + per_page + "&access_token=" + ACCESS_TOKEN).read()
+    global activities
+    activities = json.loads(contents)
 
-def get_activities():
-    for activity in data:
-        print activity["name"]
-
-def get_fields():
-    for field in data[0]:
+def output_fields():
+    for field in activities[0]:
         print field,
 
 def output_activities():
-    for activity in data:
+    for activity in activities:
         print activity["name"], activity["id"]
 
+def output_segment_ids():
+    for segment in segments:
+        print segment["id"]
+
 def populate_segments():
-    for activity in data:
+    for activity in activities:
         if activity["type"] != "Ride" or activity["manual"] == True:
             continue
-        for segment in activity["segment_efforts"]:
+        activity_id = str(activity["id"])
+        activity_data = json.loads(urllib2.urlopen("https://www.strava.com/api/v3/activities/" + activity_id + "?per_page=200&access_token=" + ACCESS_TOKEN).read())
+        for segment in activity_data["segment_efforts"]:
             if segment["id"] not in segments:
-                segments.append(segment["id"])
-                print segment["id"]
+                segments.append(segment)
 
 
 # output_activities()
+get_activities(20)
 populate_segments()
+output_segment_ids()
